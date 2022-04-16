94a2cf0510647160b7f6f75f2f049112caa25450
RudrakshaBillore1 <rudrakshabillore@gmail.com>
2022-04-16 14:06:35 +0530

    first  commit

---
+++ src/Tittle.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from './logo.png';
+
+export const Tittle = () => {
+  return (
+    <div className='Title'>
+    <img src={logo} alt="logo" /><span className='Name'>REACT CALCULATOR</span>
+    </div>
+  )
+}
